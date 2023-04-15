7bbfa7b99f07ad4c4cdd1f10436f49b6a5941ed2
George·Dong <georgedong32@foxmail.com>
2023-04-15 23:41:46 +0800

    Update

--- Experiment3/USER/main.c
+++ Experiment3/USER/main.c
@@ -14,13 +14,49 @@ void LED_GPIO_Confing(void)
 
 void MYTIM4_Init(void);
 
+void TIM4_Init(void);
+
+void TIME4_NVIC_Init(void);
+
 int main(void)
 {
     LED_GPIO_Confing();
-    MYTIM4_Init();
+    TIM4_Init();
+    TIME4_NVIC_Init();
     while (1)
     {
     }
+}
+
+void TIM4_Init(void)
+{
+    TIM_TimeBaseInitTypeDef TIM_TimeBaseStructure;
+    RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM4, ENABLE); // 配置系统时钟
+
+    // 初始化 TIM4 定时器
+    TIM_TimeBaseStructure.TIM_Period = 19999;                     // 设置在下一个更新事件装入活动的自动重装载寄存器ARR 的值, 计数值 20000
+    TIM_TimeBaseStructure.TIM_Prescaler = 3599;                   // 设置 TIM4 的预分频值 PSC 20KHz
+    TIM_TimeBaseStructure.TIM_CounterMode = TIM_CounterMode_Down; // 向下计数模式
+    TIM_TimeBaseStructure.TIM_ClockDivision = TIM_CKD_DIV1;       // 设置时钟分割
+    TIM_TimeBaseInit(TIM4, &TIM_TimeBaseStructure);               // 根据 TIM_TimeBaseInitStruc 中指定的参数初始化 TIM4
+    TIM_ClearFlag(TIM4, TIM_FLAG_Update);                         // 清除溢出中断标志
+    TIM_ITConfig(TIM4, TIM_IT_Update, ENABLE);                    // 使能 TIM4 中断
+
+    TIM_Cmd(TIM4, ENABLE); // 使能 TIM4定时
+}
+
+void TIME4_NVIC_Init(void)
+{
+    NVIC_InitTypeDef NVIC_InitStructure;
+    NVIC_PriorityGroupConfig(NVIC_PriorityGroup_2); // 中断优先级 2 位
+
+    // 设置系统中断管理参数,使能 TIM4 中断
+    NVIC_InitStructure.NVIC_IRQChannel = TIM4_IRQn;           // TIM4 中断通道
+    NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 2; // 抢占优先级 2（0~3），中断优先级
+    NVIC_InitStructure.NVIC_IRQChannelSubPriority = 2;        // 子优先级 2
+    NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;           // 中断使能
+
+    NVIC_Init(&NVIC_InitStructure); // 参数初始化 NVIC
 }
 
 void MYTIM4_Init(void)
@@ -28,23 +64,25 @@ void MYTIM4_Init(void)
     TIM_TimeBaseInitTypeDef TIM_TimeBaseStructure;
     NVIC_InitTypeDef NVIC_InitStructure;
 
-    NVIC_PriorityGroupConfig(NVIC_PriorityGroup_2);
-    RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM4, ENABLE);
+    NVIC_PriorityGroupConfig(NVIC_PriorityGroup_2);      // 中断优先级 2 位
+    RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM4, ENABLE); // 配置系统时钟
 
-    TIM_TimeBaseStructure.TIM_Period = 19999;
-    TIM_TimeBaseStructure.TIM_Prescaler = 3599;
-    TIM_TimeBaseStructure.TIM_CounterMode = TIM_CounterMode_Down;
-    TIM_TimeBaseStructure.TIM_ClockDivision = TIM_CKD_DIV1;
-    TIM_TimeBaseInit(TIM4, &TIM_TimeBaseStructure);
-    TIM_ClearFlag(TIM4, TIM_FLAG_Update);
-    TIM_ITConfig(TIM4, TIM_IT_Update, ENABLE);
+    // 初始化 TIM4 ms 定时
+    TIM_TimeBaseStructure.TIM_Period = 19999;                     // 设置在下一个更新事件装入活动的自动重装载寄存器ARR 的值, 计数值 20000
+    TIM_TimeBaseStructure.TIM_Prescaler = 3599;                   // 设置 TIM4 的预分频值 PSC 20KHz
+    TIM_TimeBaseStructure.TIM_CounterMode = TIM_CounterMode_Down; // 向下计数模式
+    TIM_TimeBaseStructure.TIM_ClockDivision = TIM_CKD_DIV1;       // 设置时钟分割
+    TIM_TimeBaseInit(TIM4, &TIM_TimeBaseStructure);               // 根据 TIM_TimeBaseInitStruc 中指定的参数初始化 TIM4
+    TIM_ClearFlag(TIM4, TIM_FLAG_Update);                         // 清除溢出中断标志
+    TIM_ITConfig(TIM4, TIM_IT_Update, ENABLE);                    // 使能 TIM4 中断
 
-    TIM_Cmd(TIM4, ENABLE);
+    // 设置系统中断管理参数,使能 TIM4 中断
+    NVIC_InitStructure.NVIC_IRQChannel = TIM4_IRQn;           // TIM4 中断通道
+    NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 2; // 抢占优先级 2（0~3），中断优先级
+    NVIC_InitStructure.NVIC_IRQChannelSubPriority = 2;        // 子优先级 2
+    NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;           // 中断使能
 
-    NVIC_InitStructure.NVIC_IRQChannel = TIM4_IRQn;
-    NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 2;
-    NVIC_InitStructure.NVIC_IRQChannelSubPriority = 2;
-    NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
+    NVIC_Init(&NVIC_InitStructure); // 参数初始化 NVIC
 
-    NVIC_Init(&NVIC_InitStructure);
+    TIM_Cmd(TIM4, ENABLE); // 使能 TIM4定时
 }
