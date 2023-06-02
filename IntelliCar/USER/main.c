345fff07956814bdc33383909ed36d7aa017c50b
George·Dong <georgedong32@foxmail.com>
2023-06-02 13:16:30 +0800

    重构Git2

---
+++ IntelliCar/USER/main.c
@@ -0,0 +1,16 @@
+#include "stm32f10x.h"                  // Device header
+#include "Motor.h"
+#include "Servo.h"
+
+int main(void)
+{
+	/*Init Zone*/
+	Motor_Init(7199,0);
+	
+	/*End Init Zone*/
+	
+	while(1)
+	{
+		
+	}
+}
